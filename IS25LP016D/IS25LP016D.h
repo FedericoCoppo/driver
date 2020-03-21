5a23bb930cfe9ef8d8a43440ac48b192ce67d7ab
Federico Coppo <54506799+FedericoCoppo@users.noreply.github.com>
2020-03-21 10:49:01 +0100

    driver for IS25LP016D.h flash.

---
+++ IS25LP016D/IS25LP016D.h
@@ -0,0 +1,19 @@
+/*
+ * IS25LP016D.h
+ *
+ *  Created on: 17-03-2020
+ *      Author: FCoppo
+ */
+
+#ifndef IS25LP016D_H_
+#define IS25LP016D_H_
+
+
+#include "HL_hal_stdtypes.h"
+
+
+
+bool IS25LP016D_TEST_ID (void);
+bool IS25LP016D_TEST_ERASE_WRITE_READ (void);
+
+#endif /* IS25LP016D_H_ */
